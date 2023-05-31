28481108d3c93c18fd4a9e0314393070917360c6
swhasans <SS3858@live.mdx.ac.uk>
2023-05-31 13:17:11 -0400

    Add db URI

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express"); // Import the Express module 
 const morgan = require("morgan"); // Import the Morgan module for logging HTTP requests
-const mongoose = require("mongoose"); // Import the Mongoose module for MongoDB connection
 const appointmentRoutes = require("./routes/appointmentRoutes.js"); // Import the appointmentRoutes module
 const bodyParser = require('body-parser'); // Import the bodyParser module for parsing JSON request bodies
+const mongoose = require("mongoose"); // Import the Mongoose module for MongoDB connection
 
 // Express app
 const app = express(); // Create an Express application
 const port = 3000; // Specify the port number for the server
 
+// Connect to MongoDB
+const dbURI = "mongodb+srv://<username>:<password>@nodeexpressapp.bl1lxtc.mongodb.net/<database_name>?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) // Connect to MongoDB using the provided URI and options
+    .then((results) => {
+        // Start the server and listen for requests on the specified port
+        app.listen(port, "localhost", () => {
+            console.log(`Listening for requests on port: ${port}`);
+        });
+    })
+    .catch((err) => console.log(err))
+
 // Middleware
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
 app.use(morgan("dev")); // Log HTTP requests in the console using Morgan
 app.use(bodyParser.json()); // Parse JSON request bodies
-
-app.listen(port, "localhost", () => {
-  console.log(`Listening for requests on port: ${port}`);
-});
 
 app.use('/', appointmentRoutes); // Use appointmentRoutes for handling routes starting with '/'
 
