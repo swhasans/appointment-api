05c699153db59346a1c9678547e7e892560164ea
swhasans <SS3858@live.mdx.ac.uk>
2023-05-31 13:02:27 -0400

    Chore: comments

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 const appointmentController = require("../controllers/appointmentControllers.js");
 
-//GET method (ALL!)
+// GET method to fetch all appointments
 router.get("/api/v1/appointments", appointmentController.getAllAppointment);
 
-// GET method
+// GET method to fetch a specific appointment by ID
 router.get("/api/v1/appointments/:id", appointmentController.getAppointment);
 
-// POST method
+// POST method to create a new appointment
 router.post("/api/v1/appointments", appointmentController.createAppointment);
 
-// PUT method
+// PUT method to update an existing appointment by ID
 router.put("/api/v1/appointments/:id", appointmentController.updateAppointment);
 
-// DELETE method
-router.delete(appointmentController.deleteAppointment);
+// DELETE method to delete an appointment by ID
+router.delete("/api/v1/appointments/:id", appointmentController.deleteAppointment);
 
 module.exports = router;
