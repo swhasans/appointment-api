ece5f8408a421cbb90cb2a44be17d681b177947b
swhasans <SS3858@live.mdx.ac.uk>
2023-06-01 10:53:03 -0400

    Fixed PUT method to update existing entries

--- controllers/appointmentControllers.js
+++ controllers/appointmentControllers.js
@@ -4,7 +4,7 @@ const appointmentsFilePath = "./routes/appointments.json";
 
 //GET method (ALL!)
 const getAllAppointment = (req, res) => {
-    Appointment.find().sort({ createdAt: -1 })
+    Appointment.find().sort({ appointment_sl_id: -1 })
         .then((result) => {
             res.send(result);
         })
@@ -48,61 +48,23 @@ const createAppointment = (req, res) => {
 // PUT method
 const updateAppointment = (req, res) => {
     const appointment_sl_id = req.params.id;
-    Appointment.findOneAndUpdate({ appointment_sl_id: appointment_sl_id })
-        .then((result) => {
-            const appointment = new Appointment({
-                doctor_id: req.body.doctor_id,
-                patient_id: req.body.patient_id,
-                duration: req.body.duration,
-                reason: req.body.reason,
-                status: req.body.status,
-                appointment_sl_id: req.body.appointment_sl_id
-            });
 
-            appointment.save()
-                .then((result) => {
-                    res.send(result);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+    const updatedAppointmentData = {
+        doctor_id: req.body.doctor_id,
+        patient_id: req.body.patient_id,
+        duration: req.body.duration,
+        reason: req.body.reason,
+        status: req.body.status,
+        appointment_sl_id: req.body.appointment_sl_id
+    };
+
+    Appointment.findOneAndUpdate({ appointment_sl_id: appointment_sl_id }, updatedAppointmentData, { new: true })
+        .then((result) => {
+            res.send(result);
         })
         .catch((err) => {
             console.log(err);
-        })
-
-    // try {
-    //     const data = fs.readFileSync(appointmentsFilePath);
-    //     const appointments = JSON.parse(data);
-    //     const appointment = appointments.find(
-    //         (appointment) => appointment.id === Number(req.params.id)
-    //     );
-    //     if (!appointment) {
-    //         const err = new Error("Appointment not found");
-    //         err.status = 404;
-    //         throw err;
-    //     }
-    //     const updatedAppointmentData = {
-    //         id: req.body.id,
-    //         doctor_id: req.body.doctor_id,
-    //         patient_id: req.body.patient_id,
-    //         appointment_time: req.body.appointment_time,
-    //         duration: req.body.duration,
-    //         reason: req.body.reason,
-    //         status: req.body.status,
-    //     };
-    //     const updatedAppointments = appointments.map((appointment) => {
-    //         if (appointment.id === Number(req.params.id)) {
-    //             return updatedAppointmentData;
-    //         } else {
-    //             return appointment;
-    //         }
-    //     });
-    //     fs.writeFileSync(appointmentsFilePath, JSON.stringify(updatedAppointments));
-    //     res.status(200).json(updatedAppointmentData);
-    // } catch (e) {
-    //     next(e);
-    // }
+        });
 };
 
 // DELETE method
