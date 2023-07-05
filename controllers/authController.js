9d35ee46e997fca8da4cba483d51bd1a1cdd8bbe
swhasans <syedwadoodhassan@gmail.com>
2023-07-05 17:10:59 -0400

    Add dummy functions in authController that are referenced by the handler in authRoutes.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,34 @@
 const User = require("../models/user.js");
 const jwt = require("jsonwebtoken");
 
+// GET method (Fetches signup page)
+const getSignUp = (req, res) => {
+    res.render('signup');
+};
+
+// GET method (Fetches a specific user by ID)
+const getLogIn = (req, res) => {
+    res.render('login');
+};
+
+// POST method (Creates a new user)
+const postSignUp = (req, res) => {
+    res.send('new signup');
+};
+
+
+const postLogIn = (req, res) => {
+    res.send('user login');
+};
+ 
+const getLogout = (req, res) => {
+	
+};
+
+module.exports = {
+    getSignUp,
+    getLogIn,
+    postSignUp,
+    postLogIn,
+    getLogout
+};
