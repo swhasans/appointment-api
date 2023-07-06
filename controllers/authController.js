a77f39245e4256972206ded198cb089b52e230c3
swhasans <syedwadoodhassan@gmail.com>
2023-07-06 15:27:52 -0400

    Send type of error in json back to browser

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,15 @@ const User = require("../models/user.js");
 // Handle Errors
 const handleErrors = (err) => {
     console.timeLog(err.message, err.code);
+    let errors = {email: "", password:""};
+    
+    if(err.message.includes("user validation failed")){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
 }
 
 // GET method (Fetches signup page)
