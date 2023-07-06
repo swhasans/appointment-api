bafb12f5e7b72985d7ebac856191406a074a12e0
swhasans <syedwadoodhassan@gmail.com>
2023-07-06 14:37:34 -0400

    Import function isEmail from validator to validate the email.

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const {isEmail} = require("validator");
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Please enter an email"],
         unique: true,
         lowercase: true,
-        validate: [(val) => {}, "Please enter a valid email"]
+        validate: [isEmail, "Please enter a valid email"]
     },
     password: {
         type: String,
