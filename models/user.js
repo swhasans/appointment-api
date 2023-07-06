d244d2dd42c9e361f771acac92ec68e0cf2fd74c
swhasans <syedwadoodhassan@gmail.com>
2023-07-06 12:24:46 -0400

    Define the user schema. Two field email and password

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+});
