f73ab2bcb3ebe1eb49c253ab64681418c37ddd4d
swhasans <syedwadoodhassan@gmail.com>
2023-07-27 12:05:09 -0400

    Assign a token to each user that sign ups, based on their user._id

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,8 +43,9 @@ const postSignUp = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.create({ email, password });
-        
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({user: user._id});
     } catch (err) {
         const error = handleErrors(err);
         res.status(400).send({error});
