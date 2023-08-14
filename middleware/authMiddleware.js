795f9fd6d259f989db8a84892ab704cedb2b002b
swhasans <syedwadoodhassan@gmail.com>
2023-08-14 12:50:15 -0400

    Verify token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
-    
-    //if jwt exists and is verified
-    if(token){
-        
-    }
-	next();
+	const token = req.cookies.jwt;
+
+	//if jwt exists and is verified
+	if (token) {
+		jwt.verify(token, "swhasan secret", (err, decodedToken) => {
+			if (err) {
+				console.log(err.message);
+				res.redirect("/login");
+			} else {
+				console.log(decodedToken);
+				next();
+			}
+		});
+	} else {
+		res.redirect("/login");
+	}
 };
+
+module.exports = { requireAuth };
